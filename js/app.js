46b269d6ce28c9317d07ce616a4a263cb6dd7a6d
SHARMARKE NUR <Snur206@yahoo.com>
2022-10-31 18:21:07 -0700

    cookie hours

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 // eslint-disable-next-line no-unused-vars
 let salesSection = document.getElementById('Sales');
 let shopHours = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
+// eslint-disable-next-line no-unused-vars
 let allShops = [];
 
 // from MDN
